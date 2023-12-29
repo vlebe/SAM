c3f24c5017bf3a46cdb1e6b5fa3709a55cc9b054
Joey SKAF <jskaf@Joeys-MacBook-Pro.local>
2023-12-29 23:28:33 +0100

    Changement d'embedding Model -> ResNet50, pas de finetuning dispo pour l'instant

--- dataset.py
+++ dataset.py
@@ -3,19 +3,37 @@ import pandas as pd
 from scipy.io import wavfile
 import os
 import numpy as np
-import cv2
 import torch
+from PIL import Image
+from torchvision import transforms
 
-resolution = (70, 126, 3)
+
+resolution = (3,256,256)
 number_of_frames = 4
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
 
 class Dataset(Dataset):
-    def __init__(self, labels_file_path, frame_dir, audio_dir, txt_data_file_path, img_shape):
+    def __init__(self, labels_file_path, frame_dir, audio_dir, txt_data_file_path, img_shape, preprocess = None):
         self.labels = pd.read_csv(labels_file_path)
         self.frame_dir = frame_dir
         self.audio_dir = audio_dir
         self.img_shape = img_shape
         self.txt_data = pd.read_csv(txt_data_file_path)['text']
+        if preprocess is None :
+            self.preprocess = transforms.Compose([
+                transforms.Pad(padding=256).to(device=device),
+                transforms.Resize(256).to(device=device),
+                transforms.CenterCrop(256).to(device=device),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]).to(device=device),
+            ])
+        else :
+            self.preprocess = preprocess
 
     def __len__(self) :
         return len(self.labels)
@@ -37,8 +55,10 @@ class Dataset(Dataset):
 
         frame_sequence = np.zeros((4, self.img_shape[0], self.img_shape[1], self.img_shape[2]))
         for i, image_path in enumerate(img_list_path):
-            img = cv2.imread(frame_dir + image_path)
-            frame_sequence[i] = cv2.resize(img, (self.img_shape[1], self.img_shape[0]))
+            img = Image.open(frame_dir + image_path) 
+            img = transforms.Pad(padding=256)(img)
+            img = self.preprocess(img)
+            frame_sequence[i] = img
 
         # audio, frame_sequence = torch.tensor(audio), torch.tensor(frame_sequence)
         frame_sequence = torch.tensor(frame_sequence, dtype=torch.float32)
@@ -47,4 +67,4 @@ class Dataset(Dataset):
     
 if __name__ == "__main__" :
     dataset = Dataset('labels.csv', 'data/video/dataset_frame/', 'data/audio/samples/', 'txt_data.csv', resolution)
-    print(dataset.__get_item__(0))
+    print(dataset.__getitem__(0))
--- model_video.py
+++ model_video.py
@@ -7,6 +7,10 @@ from dataset import Dataset
 from tqdm import tqdm
 import os
 import argparse
+import matplotlib.pyplot as plt
+import torchvision 
+
+    
 
 class VideoEmbedding(nn.Module):
     def __init__(self, pretrained_model, layers_fine_tuned = None):
@@ -34,7 +38,7 @@ class VideoEmbedding(nn.Module):
                 frozen_layers.append([name, param])
             else:
                 learnable_layers.append([name, param])
-        with open('architecture_our_model.txt', 'w') as f :
+        with open('architecture_our_embedding_model.txt', 'w') as f :
             f.write('--------------------------FROZEN LAYERS--------------------------' + '\n')
             for layer in frozen_layers:
                 f.write(str(layer[0]) + '\n')
@@ -45,7 +49,6 @@ class VideoEmbedding(nn.Module):
                 f.write(str(layer[1]) + '\n')
 
     def forward(self, x):
-        x = x.permute(0, 3, 1, 2)
         x = self.pretrained_model(x)
         size_dim1, size_dim2, size_dim3 = x.size(1), x.size(2), x.size(3)
         x = torch.reshape(x, (x.size(0), size_dim1 * size_dim2 * size_dim3))
@@ -56,7 +59,14 @@ class VideoModelLateFusion(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(VideoModelLateFusion,self).__init__()
         self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+        self.fc1 = nn.Linear(hidden_size, hidden_size//2)
+        self.batchnorm1 = nn.BatchNorm1d(hidden_size//2)
+        # self.fc2 = nn.Linear(hidden_size//2, hidden_size//4)
+        # self.batchnorm2 = nn.BatchNorm1d(hidden_size//4)
+        self.fo = nn.Linear(hidden_size//2, num_classes)
+        self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(p=0.3)
+        # self.dropout2 = nn.Dropout(p=0.5)
     
     def get_architecture(self):
         learnable_layers = []
@@ -70,11 +80,21 @@ class VideoModelLateFusion(nn.Module):
 
     def forward(self, x):
         x, _ = self.gru(x)
-        x = self.fc(x[:, -1, :])  # Take the last output from the GRU sequence
+        x = self.fc1(x[:, -1, :])  # Take the last output from the GRU sequence
+        x = self.batchnorm1(x)
+        x = self.relu(x)
+        x = self.dropout1(x)
+        # x = self.fc2(x)
+        # x = self.batchnorm2(x)
+        # x = self.relu(x)
+        # x = self.dropout2(x)
+        x = self.fo(x)
         return x
 
 def train(embedding_model, model, train_loader, optimizer, criterion, output_embedding_model_shape):
+
     print("Training...")
+    embedding_model.train()
     model.train()
     running_loss = 0.0
     for i, (labels, _, frames) in tqdm(enumerate(train_loader)):
@@ -94,24 +114,60 @@ def train(embedding_model, model, train_loader, optimizer, criterion, output_emb
         running_loss += loss.item()
     return running_loss / len(train_loader)
 
-def test(embedding_model, model, test_loader, criterion, output_embedding_model_shape):
+def test(embedding_model, model, test_loader, criterion, output_embedding_model_shape, threshold=0.7):
+    global device
     print("Testing...")
+    embedding_model.eval()
     model.eval()
     running_loss = 0.0
+    total_predictions = torch.tensor([], dtype=torch.float32, device = device)
+    total_labels = torch.tensor([], dtype=torch.float32, device = device)
+
     with torch.no_grad():
         for i, (labels, _, frames) in tqdm(enumerate(test_loader)):
-            sequence = torch.zeros((1,4, output_embedding_model_shape[1] * output_embedding_model_shape[2] * output_embedding_model_shape[3]))
+            sequence = torch.zeros((1, 4, output_embedding_model_shape[1] * output_embedding_model_shape[2] * output_embedding_model_shape[3]), dtype=torch.float32)
+            
             if torch.backends.mps.is_available():
                 labels = labels.to('mps')
                 frames = frames.to('mps')
                 sequence = sequence.to('mps')
-            labels = 1.0 - 2*labels.view(-1, 1).float()
+            
+            labels = 1.0 - 2 * labels.view(-1, 1).float()
+
             for j in range(frames.shape[1]):
-                sequence[:,j, :] = embedding_model(frames[:,j, :, :, :])
+                sequence[:, j, :] = embedding_model(frames[:, j, :, :, :])
+
             outputs = model(sequence)
             loss = criterion(outputs, labels)
             running_loss += loss.item()
-    return running_loss / len(test_loader)
+
+            # Concatenate predictions and labels for later metric calculation
+            total_predictions = torch.cat((total_predictions, outputs.view(-1)))
+            total_labels = torch.cat((total_labels, labels.view(-1)))
+
+            # Calculate precision, recall, and accuracy for the current batch
+            binary_predictions = torch.argmax(torch.sigmoid(outputs), dim=1)
+            tp = torch.sum((binary_predictions == 1) & (labels == 1)).item()
+            fp = torch.sum((binary_predictions == 1) & (labels == 0)).item()
+            tn = torch.sum((binary_predictions == 0) & (labels == 0)).item()
+            fn = torch.sum((binary_predictions == 0) & (labels == 1)).item()
+
+
+    average_loss = running_loss / len(test_loader)
+    thresholded_predictions = torch.where(torch.sigmoid(total_predictions) > threshold, torch.tensor(1.0, device=device), torch.tensor(0.0, device=device))
+    # Calculate overall precision, recall, and accuracy
+    overall_tp = torch.sum((thresholded_predictions == 1) & (total_labels == 1)).item()
+    overall_fp = torch.sum((thresholded_predictions == 1) & (total_labels == 0)).item()
+    overall_tn = torch.sum((thresholded_predictions == 0) & (total_labels == 0)).item()
+    overall_fn = torch.sum((thresholded_predictions == 0) & (total_labels == 1)).item()
+
+    overall_precision = overall_tp / max(overall_tp + overall_fp, 1e-10)
+    overall_recall = overall_tp / max(overall_tp + overall_fn, 1e-10)
+    overall_accuracy = (overall_tp + overall_tn) / max(overall_tp + overall_tn + overall_fp + overall_fn, 1e-10)
+    overall_f1_score = 2 * overall_precision * overall_recall / max(overall_precision + overall_recall, 1e-10)
+
+    print(f'Overall Metrics - Loss: {average_loss:.4f}, Precision: {overall_precision:.4f}, Recall: {overall_recall:.4f}, Accuracy: {overall_accuracy:.4f} F1 Score: {overall_f1_score:.4f}')
+    print(f"Test loss {average_loss}")
 
 
 
@@ -124,16 +180,15 @@ if __name__ == '__main__':
     num_cores = os.cpu_count()
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=32, help='input batch size')
-    parser.add_argument('--num_epochs', type=int, default=10, help='number of epochs to train for')
+    parser.add_argument('--num_epochs', type=int, default=50, help='number of epochs to train for')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
-    parser.add_argument('--learning_rate_fine_tuned_layers', type=float, default=0.0001, help='learning rate for fine-tuned layers')
-    parser.add_argument('--learning_rate_default', type=float, default=0.001, help='learning rate for other layers')
+    parser.add_argument('--learning_rate_default', type=float, default=0.0001, help='learning rate for other layers')
     parser.add_argument('--hidden_size', type=int, default=512, help='hidden size')
     parser.add_argument('--num_layers', type=int, default=1, help='number of layers')
-    parser.add_argument('--output_embedding_model_shape', type=tuple, default=(32, 1280, 3, 4), help='output shape of the embedding model')
+    parser.add_argument('--max_iter', type=int, default=10, help='maximum number of iteration, witout any improvement on the train loss with tol equal to 1e-3')
+    parser.add_argument('--output_embedding_model_shape', type=tuple, default=(32, 2048, 1, 1), help='output shape of the embedding model')
     parser.add_argument('--num_classes', type=int, default=1, help='number of classes')
-    parser.add_argument('--resolution', type=tuple, default=(70, 126, 3), help='resolution of the input images')
-    parser.add_argument('--nb_layers_fine_tuned', type=int, default=0, help='number of fine tuned layers of the pretrained model')
+    parser.add_argument('--resolution', type=tuple, default=(3, 224, 224), help='resolution of the input images')
     parser.add_argument('--num_workers', type=int, default=6, help='number of workers, corresponding to number of CPU cores that want to be used for training and testing. 6 is recommended if available.')
     args = parser.parse_args()
     batch_size = args.batch_size
@@ -147,44 +202,21 @@ if __name__ == '__main__':
         device = torch.device('cpu')
     print(f"Using device {device}.")
 
-    pretrained_model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
-    pretrained_model.to(device)
-    total_nb_pt_layers = len(pretrained_model.features)
-    nb_pt_layers_to_fine_tune = total_nb_pt_layers - args.nb_layers_fine_tuned
-    if nb_pt_layers_to_fine_tune == total_nb_pt_layers:
-        layers_fine_tuned = None
-        layers_fine_tuned_name = None
-    else:
-        layers_fine_tuned = pretrained_model.features[nb_pt_layers_to_fine_tune:]
-        layers_fine_tuned_name = [name for name, param in layers_fine_tuned.named_parameters()]
+    weights = torchvision.models.ResNet50_Weights.IMAGENET1K_V2
+    pretrained_model = torchvision.models.resnet50(weights=weights)
+    pretrained_model = pretrained_model.to(device) 
+    preprocess = weights.transforms().to(device)
 
-    if layers_fine_tuned is not None:
-        print(f"Fine-tuning {nb_pt_layers_to_fine_tune} layers out of {total_nb_pt_layers} layers of the pretrained model.")
-        layers_fine_tuned.to(device)
-    else:
-        print("Not fine-tuning any layer of the pretrained model.")
-
-
-    embedding_model = VideoEmbedding(pretrained_model, layers_fine_tuned)
+    embedding_model = VideoEmbedding(pretrained_model)
     model = VideoModelLateFusion(input_size, args.hidden_size, args.num_layers, args.num_classes)
     embedding_model.to(device)
     model.to(device)
 
-    params = []
-    for name, param in model.named_parameters():
-        if param.requires_grad:
-            if layers_fine_tuned is None:
-                params.append({'params': param, 'lr': args.learning_rate_default})
-            else:
-                if name is not None and name in layers_fine_tuned_name:
-                    params.append({'params': param, 'lr': args.learning_rate_fine_tuned_layers})
-                else:
-                    params.append({'params': param, 'lr': args.learning_rate_default})
-    optimizer = optim.Adam(params=params)
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
     criterion = nn.BCEWithLogitsLoss()
     criterion.to(device)
 
-    dataset = Dataset('labels.csv', 'data/video/dataset_frame/', 'data/audio/samples/', 'txt_data.csv', args.resolution)
+    dataset = Dataset('labels.csv', 'data/video/dataset_frame/', 'data/audio/samples/', 'txt_data.csv', args.resolution, preprocess)
     indice1 = torch.randperm(1116)
     indice2 = torch.randperm(len(dataset)- 1984) + 1984
     indice = torch.cat((indice1,indice2))
@@ -199,21 +231,37 @@ if __name__ == '__main__':
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
         test_loader = torch.utils.data.DataLoader(test_dataset, shuffle=True)
 
+    # for batch in train_loader:
+    #     labels, _, frames = batch
+    #     plt.imshow(frames[0, 0, :, :, :].permute(1, 2, 0))
+    #     plt.show()
+    #     break
+
     print("Start training...")
+    old_train_loss = 0
+    iteration = 0
     for epoch in range(args.num_epochs):
         train_loss = train(embedding_model, model, train_loader, optimizer, criterion, args.output_embedding_model_shape)
+        if old_train_loss - train_loss < 1e-3:
+            iteration += 1
+        else:
+            iteration = 0
+        if iteration == args.max_iter or old_train_loss - train_loss < 0.0 :
+            print(f"Early stopping at epoch {epoch+1} : train loss {train_loss}")
+            break
         print(f"Epoch {epoch+1} : train loss {train_loss}")
 
 
-    torch.save(embedding_model.state_dict(), 'embedding_model.pt')
-    torch.save(model.state_dict(), 'model.pt')
+    torch.save(embedding_model.state_dict(), 'data/parameter_models/embedding_modelV3.pt')
+    torch.save(model.state_dict(), 'data/parameter_models/modelV3.pt')
 
-    # model = VideoModelLateFusion(input_size, hidden_size, num_layers, num_classes)
-    # model.load_state_dict(torch.load('model.pt'))
-    # embedding_model = VideoEmbedding(pretrained_model, layers_fine_tuned)
-    # embedding_model.load_state_dict(torch.load('embedding_model.pt'))
-    test_loss = test(embedding_model, model, test_loader, criterion, args.output_embedding_model_shape)
-    print(f"Test loss {test_loss}")
+    # model = VideoModelLateFusion(input_size, args.hidden_size, args.num_layers, args.num_classes).to(device)
+    # model.load_state_dict(torch.load('data/parameter_models/modelV3.pt'))
+    # embedding_model = VideoEmbedding(pretrained_model, layers_fine_tuned).to(device)
+    # embedding_model.load_state_dict(torch.load('data/parameter_models/embedding_modelV3.pt'))
+        
+
+    test(embedding_model, model, test_loader, criterion, args.output_embedding_model_shape)
 
 
 
